09d98c9edddbd772516d4c6e676b4b88d792e581
d-inagaki <chanc.euse.0302@gmail.com>
2019-04-11 22:46:34 +0900

    flywayのmigrationファイルにコメントを追加した

--- apps/kotlindemo/src/main/resources/db/migration/V1__createUserTable.sql
+++ apps/kotlindemo/src/main/resources/db/migration/V1__createUserTable.sql
@@ -1,11 +1,11 @@
 -- users
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `status` tinyint(4) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT, --ID
+  `first_name` varchar(255) NOT NULL,        --名
+  `last_name` varchar(255) NOT NULL,         --姓
+  `email` varchar(255) NOT NULL,             --メールアドレス
+  `password` varchar(255) NOT NULL,          --パスワード
+  `status` tinyint(4) NOT NULL,              --ステータス
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`user_id`)
