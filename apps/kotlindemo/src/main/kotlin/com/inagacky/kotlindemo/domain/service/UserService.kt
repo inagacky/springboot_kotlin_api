66b5544dd94fe15aa10833e74a34de5cacb1fb8a
d-inagaki <chanc.euse.0302@gmail.com>
2019-04-15 14:56:18 +0900

    ユーザー取得のAPIを追加した

--- apps/kotlindemo/src/main/kotlin/com/inagacky/kotlindemo/domain/service/UserService.kt
+++ apps/kotlindemo/src/main/kotlin/com/inagacky/kotlindemo/domain/service/UserService.kt
@@ -3,6 +3,7 @@ package com.inagacky.kotlindemo.domain.service
 import com.inagacky.kotlindemo.domain.entity.sample.User
 import com.inagacky.kotlindemo.exception.IllegalDataException
 import com.inagacky.kotlindemo.exception.SampleSQLException
+import com.inagacky.kotlindemo.exception.ValidationException
 
 /**
  * User API Service
@@ -17,7 +18,7 @@ interface UserService : BaseService {
      *
      * @return
      */
-    @Throws(SampleSQLException::class, IllegalDataException::class)
+    @Throws(SampleSQLException::class, ValidationException::class)
     fun createTmpUser(user: User)
 
     /**
@@ -28,6 +29,17 @@ interface UserService : BaseService {
      *
      * @return
      */
-    @Throws(SampleSQLException::class, IllegalDataException::class)
+    @Throws(SampleSQLException::class, ValidationException::class)
     fun updateUser(paramUser: User): User
+
+    /**
+     *
+     * ユーザーの取得処理
+     *
+     * @param userId
+     *
+     * @return
+     */
+    @Throws(SampleSQLException::class, IllegalDataException::class)
+    fun findUser(userId: Int): User
 }
